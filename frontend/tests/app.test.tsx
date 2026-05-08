d17adf376220502143053f642b3a2a9bf1f92006
Laurynas Duburas <laduem@gmail.com>
2026-05-08 21:52:57 +0300

    Initial commit

---
+++ frontend/tests/app.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { createRoutesStub } from "react-router";
+import { expect, test } from "vitest";
+
+
+test("renders auxiliary component correctly", async () => {
+  expect("dummy").toBeDefined();
+});
